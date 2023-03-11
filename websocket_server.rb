a34e6bf044e2360dfab14a99e79a86a722960d5a
vs256 <44126680+vs256@users.noreply.github.com>
2023-03-11 00:46:38 -0800

    Added being able to accept the handhsake & adding a websocket key

--- websocket_server.rb
+++ websocket_server.rb
@@ -1,4 +1,7 @@
 require 'socket'
+require 'digest/sha1'
+require 'base64'
+
 
 class WebSocketServer
 
@@ -10,6 +13,34 @@ class WebSocketServer
     def accept
         socket = @tcp_server.accept
         send_handshake(socket) && WebSocketConnection.new(socket)
+    end
+
+    def send_handshake(socket)
+        request_line = socket.gets
+        header = get_header(socket)
+        if (request_line =~ /GET #{@path} HTTP\/1.1/) && (header =~ /Sec-WebSocket-Key: (.*)\r\n/)
+            ws_accept = create__accept($1)
+            #complete the handshake
+        end
+        send_400(socket)
+        false # reject the handshake
+
+    end
+
+    WS_MAGIC_STRING = "258EAFA5-E914-47DA-95CA-C5AB0DC85B11"
+
+
+    def send_400(socket)
+        socket << "HTTP/1.1 400 Bad Request\r\n" +
+                  "Content-Type: text/plain\r\n" +
+                  "Connection: close\r\n" +
+                  "\r\n" +
+                  "Incorrect request"
+        socket.close
+    end
+    
+    def get_header(socket, header = "") #gets the header by recursively reading each line offered by the socket
+        (line = socket.gets) == "rn" ? header : get_header(socket, header + line)
     end
 
 end
