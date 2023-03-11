064f597a8fcb6803a9a2a389a44acf2b38966538
vs256 <44126680+vs256@users.noreply.github.com>
2023-03-11 02:21:53 -0800

    added recv logic for interpretation of frame bytes

--- websocket_connection.rb
+++ websocket_connection.rb
@@ -6,4 +6,30 @@ class WebSocketConnection
     def initialize(socket)
         @socket = socket
     end
+
+    def recv
+        fin_and_opcode = socket.reada(1).bytes[0] #get the 0th item of [129]
+        mask_and_length_indicator = socket.read(1).bytes[0]
+        length_indicator = mask_and_length_indicator - 128
+
+        length = if length_indicator <= 125
+                    length_indicator
+                 elsif length_indicator == 126
+                    socket.read(2).unpack("n")[0]
+                  else
+                    socket.read(8).unpack("Q>")[0]
+                  end
+
+        keys = socket.read(4).bytes
+        encoded = socket.read(length).bytes
+
+        decoded = encoded.each_with_index.map do |byte, index|
+            byte ^ keys[index % 4]
+          end
+
+          decoded.pack("c*")
+    end
+
+
+
 end
